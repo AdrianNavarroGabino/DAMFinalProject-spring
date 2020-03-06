561cd77906471e70d0697edb69a1d44ef395b5fc
Adrián Navarro Gabino <adriannavarrogabino@gmail.com>
2020-03-06 15:34:45 +0100

    Project creation

---
+++ libros-backend/src/main/java/com/adriannavarrogabino/LibrosBackendApplication.java
@@ -0,0 +1,13 @@
+package com.adriannavarrogabino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrosBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibrosBackendApplication.class, args);
+	}
+
+}

fc3697193cbb617665d999ad361b8dde69070c51
Adrian Navarro Gabino <adriannavarrogabino@gmail.com>
2020-03-07 16:24:27 +0100

    Entities 2

--- libros-backend/src/main/java/com/adriannavarrogabino/models/entity/Autor.java
+++ libros-backend/src/main/java/com/adriannavarrogabino/models/entity/Autor.java
@@ -1,5 +1,57 @@
 package com.adriannavarrogabino.models.entity;
 
-public class Autor {
+import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "autores")
+public class Autor implements Serializable {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotEmpty(message = "El nombre no puede estar vacío")
+	@Size(min = 3, max = 40, message = "El tamaño del nombre tiene que estar entre 3 y 40")
+	@Column(nullable = false)
+	private String nombre;
+
+	private String nacionalidad;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getNacionalidad() {
+		return nacionalidad;
+	}
+
+	public void setNacionalidad(String nacionalidad) {
+		this.nacionalidad = nacionalidad;
+	}
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 }
--- libros-backend/src/main/java/com/adriannavarrogabino/models/entity/Estanteria.java
+++ libros-backend/src/main/java/com/adriannavarrogabino/models/entity/Estanteria.java
@@ -1,5 +1,60 @@
 package com.adriannavarrogabino.models.entity;
 
-public class Estanteria {
+import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+
+@Entity
+@Table(name = "estanterias")
+public class Estanteria implements Serializable {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotEmpty(message = "El nombre de la estantería no puede estar vacío")
+	@Column(nullable = false)
+	private String nombre;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "id_usuario")
+	private Usuario usuario;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 }

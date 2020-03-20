bd3d03f2d12e69b0bb74e3d3ee40c2e1f86b6c72
Adrián Navarro Gabino <adriannavarrogabino@gmail.com>
2020-03-20 08:30:40 +0100

    Update and delete

--- libros-backend/src/main/java/com/adriannavarrogabino/controllers/LibroRestController.java
+++ libros-backend/src/main/java/com/adriannavarrogabino/controllers/LibroRestController.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -41,5 +43,25 @@ public class LibroRestController {
 	public Libro create(@RequestBody Libro libro)
 	{
 		return libroService.save(libro);
+	}
+	
+	@PutMapping("/libros/{id}")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Libro update(@RequestBody Libro libro, @PathVariable Long id)
+	{
+		Libro libroActual = libroService.findById(id);
+		
+		libroActual.setTitulo(libro.getTitulo());
+		libroActual.setEditorial(libro.getEditorial());
+		libroActual.setFechaPublicacion(libro.getFechaPublicacion());
+		
+		return libroService.save(libroActual);
+	}
+	
+	@DeleteMapping("/libros/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void delete(@PathVariable Long id)
+	{
+		libroService.delete(id);
 	}
 }
